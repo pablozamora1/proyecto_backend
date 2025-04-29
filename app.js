553bc715ee21038dee4a88bb3e56d7c555f272dd
Pablo Zamora <pablo.zamora@icloud.com>
2025-04-29 03:44:24 -0300

    2

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+const { triggerAsyncId } = require("async_hooks");
+const { title } = require("process");
+
+const fs = require("fs").promises;
+
 class ProductManager {
   //variable estatica
   static ultID = 0;
-  constructor() {
+  constructor(path) {
     this.products = [];
+    this.path = path;
   }
   // metodos
 
-  addProduct(title, description, price, thumbnail, code, stock) {
+  async addProduct(newObjet) {
+    let { title, description, price, thumbnail, code, stock } = newObjet;
     //validacion que esten todos los campos agregados
 
     if (!title || !description || !price || !thumbnail || !code || !stock) {
@@ -38,6 +45,9 @@ class ProductManager {
     // lo subimos al array
 
     this.products.push(newProduct);
+
+    // guardar el arrray en un archivo
+    await this.saveFiles(this.products);
   }
   // metodo para traer los array
   getProducts() {
@@ -45,78 +55,111 @@ class ProductManager {
   }
 
   // buscar producto por ID
-
-  getProductById(id) {
-    const product = this.products.find((item) => item.id === id);
-    if (!product) {
-      console.log("producto no encontrado");
-    } else {
-      console.log("producto encontrado", product);
+  async getProductById(id) {
+    const arrayProducts = await this.readFlies();
+    try {
+      const product = arrayProducts.find((item) => item.id === id);
+      if (!product) {
+        console.log("producto no encontrado");
+      } else {
+        console.log("producto encontrado", product);
+      }
+      return product;
+    } catch (error) {
+      console.log("error al buscar el producto", error);
     }
-    return product;
+  }
+
+  async readFlies() {
+    try {
+      const res = await fs.readFile(this.path, "utf-8");
+      const arrayProduct = JSON.parse(res);
+      return arrayProduct;
+    } catch (error) {
+      console.log("error leer el archivo", error);
+    }
+  }
+
+  async saveFiles(arrayProduct) {
+    try {
+      await fs.writeFile(this.path, JSON.stringify(arrayProduct, null, 2));
+    } catch (error) {
+      console.log("error al guardar el archivo", error);
+    }
+  }
+
+  // actualizar archivo
+
+  async updateProduct(id, productUpdated) {
+    try {
+      const arrayProduct = await this.readFlies();
+      const index = arrayProduct.findIndex((item) => item.id === id);
+      if (index !== -1) {
+        arrayProduct.splice(index, 1, productUpdated);
+        await this.saveFiles(arrayProduct);
+      } else {
+        console.log("no se encontro el producto a actualizar");
+      }
+    } catch (error) {}
   }
 }
 
-// testing
+// // testing
 
-const manager = new ProductManager();
+const manager = new ProductManager("./products.json");
 
-manager.getProducts();
+// manager.getProducts();
 
-// manager.addProduct(
-//   "producto prueba",
-//   "este es un producto prueba",
-//   200,
-//   "sin imagen",
-//   "abc123",
-//   25
-// );
+const mouse = {
+  title: "mouse",
+  description: "blanco",
+  price: 2000,
+  thumbnail: "sin imagen",
+  code: "abc123",
+  stock: 20,
+};
+const telefono = {
+  title: "iphone",
+  description: "16 ProMax",
+  price: 5000,
+  thumbnail: "sin imagen",
+  code: "abc124",
+  stock: 30,
+};
+const notebook = {
+  title: "asus",
+  description: "gamer",
+  price: 15000,
+  thumbnail: "sin imagen",
+  code: "abc125",
+  stock: 30,
+};
 
-manager.addProduct(
-  "producto prueba",
-  "este es un producto prueba",
-  25,
-  "sin imagen",
-  "abc123",
-  20
-);
 
-manager.addProduct(
-  "producto prueba",
-  "este es un producto prueba",
-  25,
-  "sin imagen",
-  "abc123",
-  20
-);
-manager.addProduct(
-  "producto prueba",
-  "este es un producto prueba",
-  25,
-  "sin imagen",
-  "abc124",
-  20
-);
+const cargador = {
+  id: 2,
+  title: "cargador",
+  description: "iphone",
+  price: 15000,
+  thumbnail: "sin imagen",
+  code: "abc129",
+  stock: 30,
+};
+// manager.addProduct(mouse);
+// manager.addProduct(telefono);
+// manager.addProduct(notebook);
 
-manager.addProduct(
-  "telefono",
-  "iphone 16 pro max",
-  27,
-  "sin imagen",
-  "abc125",
-  20
-);
+async function testBusquedaId() {
+  const product = await manager.getProductById(3);
+  console.log(product);
+}
 
-manager.addProduct(
-  "palillos",
-  "baratos de madera",
-  2,
-  "sin imagen",
-  "abc128",
-  20
-);
+async function testUpdate() {
+  await manager.updateProduct(2, cargador);
+}
+testUpdate();
+testBusquedaId();
 
-manager.addProduct("llave", "cronos", 8, "sin imagen", "abc130", 21);
 
-manager.getProducts();
-manager.getProductById(30);
+// manager.getProducts();
+// manager.getProductById(30);
